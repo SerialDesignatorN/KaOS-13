4a70df230bf4441523b37788d54bc99919672471
zeankundev <zeanfender11@gmail.com>
2022-02-24 08:42:20 +0700

    initial commit

---
+++ Settings.js
@@ -0,0 +1,36 @@
+//background
+function bg1() {
+   document.getElementById("body").style.backgroundImage = "url(bg.jpg)";
+}
+function bg2() {
+   document.getElementById("body").style.backgroundImage = "url(citywater.png)";
+}
+function bg3() {
+   document.getElementById("body").style.backgroundImage = "url(Flowers.png)";
+}
+function bg4() {
+   document.getElementById("body").style.backgroundImage = "url(Boardwalk.png)";
+}
+function custom_bg(event) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    document.getElementById("body").style.backgroundImage = `url(${reader.result})`;
+    document.getElementById("body").style.backgroundSize = 'cover';
+  }
+  reader.readAsDataURL(event.target.files[0]);
+}
+
+//Color scheme
+
+function color1() {
+  document.getElementById("header").style.backgroundColor = "#1d1d1d";
+}
+function color2() {
+  document.getElementById("header").style.backgroundColor = "red";
+}
+function color3() {
+  document.getElementById("header").style.backgroundColor = "green";
+}
+function color4() {
+  document.getElementById("header").style.backgroundColor = "lightgray";
+}
---
+++ close.js
@@ -0,0 +1,90 @@
+function Closetestwindow() {
+  var x = document.getElementById("window");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow2() {
+  var x = document.getElementById("window2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    var mememaker = document.getElementById("mememaker"); mememaker.src = mememaker.src;
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow3() {
+  var x = document.getElementById("window3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow4() {
+  var x = document.getElementById("window4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow5() {
+  var x = document.getElementById("window5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    var store = document.getElementById("store"); store.src = ("store.html");
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow6() {
+  var x = document.getElementById("window6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function menu() {
+  var x = document.getElementById("Sidebar");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Closetestwindow7() {
+  var x = document.getElementById("window7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function CloseVSCode() {
+  var x = document.getElementById("window8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function CloseTerminal() {
+  var x = document.getElementById("window9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function NotificationMenu() {
+  var x = document.getElementById("Sidebar2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
