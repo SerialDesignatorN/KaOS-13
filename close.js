20829d0725dcdaa4126bffb6c01b84dbe0dadd05
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2022-02-25 08:36:53 +0700

    a

--- close.js
+++ close.js
@@ -2,17 +2,20 @@ function Closetestwindow() {
   var x = document.getElementById("window");
   if (x.style.display === "none") {
     x.style.display = "block";
+    var files = document.getElementById("files"); files.src = ("medias/");
   } else {
     x.style.display = "none";
+    var files = document.getElementById("files"); files.src = ("");
   }
 }
 function Closetestwindow2() {
   var x = document.getElementById("window2");
   if (x.style.display === "none") {
     x.style.display = "block";
-    var mememaker = document.getElementById("mememaker"); mememaker.src = mememaker.src;
+    var mememaker = document.getElementById("mememaker"); mememaker.src = ("apps/MEMEMAKER.html");
   } else {
     x.style.display = "none";
+    var mememaker = document.getElementById("mememaker"); mememaker.src = ("");
   }
 }
 function Closetestwindow3() {
@@ -68,16 +71,20 @@ function CloseVSCode() {
   var x = document.getElementById("window8");
   if (x.style.display === "none") {
     x.style.display = "block";
+    var vscode = document.getElementById("vscode"); vscode.src = ("https://vscode.dev");
   } else {
     x.style.display = "none";
+    var vscode = document.getElementById("vscode"); vscode.src = ("");
   }
 }
 function CloseTerminal() {
   var x = document.getElementById("window9");
   if (x.style.display === "none") {
     x.style.display = "block";
+    var terminal = document.getElementById("terminal"); terminal.src = ("terminal.html");
   } else {
     x.style.display = "none";
+    var terminal = document.getElementById("terminal"); terminal.src = ("");
   }
 }
 function NotificationMenu() {
